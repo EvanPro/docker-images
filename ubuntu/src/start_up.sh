3eba0ae0fbb87b2a197331e98d898feebfa48b6c
Meng Wang <wangmengpro@163.com>
2019-10-11 11:05:17 +0800

    Update start_up.sh

--- ubuntu/src/start_up.sh
+++ ubuntu/src/start_up.sh
@@ -5,7 +5,7 @@ set -e
 ## fix network config
 if [ -d "/root/dockerstartup/networkbackup/" ]; then
     cat /root/dockerstartup/networkbackup/hosts >> /etc/hosts
-    cat /root/dockerstartup/networkbackup/hostname /etc/hostname
+    cat /root/dockerstartup/networkbackup/hostname >> /etc/hostname
     cat /root/dockerstartup/networkbackup/resolv.conf >> /etc/resolv.conf
 fi
 
