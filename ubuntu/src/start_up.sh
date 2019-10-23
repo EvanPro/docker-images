6957357e9e94e6a9377ec0e6e694d881a4075bb1
Meng Wang <wangmengpro@163.com>
2019-10-23 15:50:16 +0800

    Update start_up.sh

--- ubuntu/src/start_up.sh
+++ ubuntu/src/start_up.sh
@@ -2,9 +2,6 @@
 ### every exit != 0 fails the script
 set -e
 
-ENABLE_SSH=${ENABLE_SSH:-true}
-ENABLE_VNC=${ENABLE_VNC:-true}
-
 ## fix network config
 if [ -d "/root/dockerstartup/networkbackup/" ]; then
     cat /root/dockerstartup/networkbackup/hosts > /etc/hosts
@@ -12,8 +9,10 @@ if [ -d "/root/dockerstartup/networkbackup/" ]; then
     cat /root/dockerstartup/networkbackup/resolv.conf > /etc/resolv.conf
 fi
 
+ENABLE_SSH=${ENABLE_SSH:-true}
+ENABLE_VNC=${ENABLE_VNC:-true}
+
 SSH_CONF=/etc/supervisor/conf.d/sshd.conf
-VNC_CONF=/etc/supervisor/conf.d/vncserver.conf
 
 # start ssh
 if [[ "$ENABLE_SSH" == "true" ]] ; then
@@ -24,19 +23,30 @@ fi
 
 # start vnc server
 if [[ "$ENABLE_VNC" == "true" ]] ; then
-    if [ ! -f $VNC_CONF ] ; then
-        echo "remove old vnc locks to be a reattachable container"
-        vncserver -kill :1 \
-            || rm -rfv /tmp/.X*-lock /tmp/.X11-unix \
-            || echo "no locks present"
+    ## change vnc password
+    echo -e "\n------------------ change VNC password  ------------------"
+    # first entry is control, second is view (if only one is valid for both)
+    mkdir -p "$HOME/.vnc"
+    PASSWD_PATH="$HOME/.vnc/passwd"
 
-        cp /root/dockerstartup/modes/vncserver.conf /etc/supervisor/conf.d/
-        sed -i "s/1152/${WIDTH:-1152}/g" $VNC_CONF
-        sed -i "s/864/${HEIGHT:-864}/g" $VNC_CONF
+    if [[ -f $PASSWD_PATH ]]; then
+        echo -e "\n---------  purging existing VNC password settings  ---------"
+        rm -f $PASSWD_PATH
     fi
+
+    echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
+    chmod 600 $PASSWD_PATH
+
+    echo -e "\n------------------ start VNC server ------------------------"
+    echo "remove old vnc locks to be a reattachable container"
+    vncserver -kill :1 \
+        || rm -rfv /tmp/.X*-lock /tmp/.X11-unix \
+        || echo "no locks present"
+    vncserver -geometry ${WIDTH:-1152}x${HEIGHT-864} :1 securitytypes=none
 fi
 
-# Forward SIGTERM to supervisord process
+
+#start supervisor
 _term() {
     while kill -0 $child >/dev/null 2>&1
     do
